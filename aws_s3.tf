3c568f9e0c7d2258c9598197709a9b547f6de577
Richard Gutkowski <richard@ososolution.com>
2019-04-01 17:34:48 +0100

    .

--- aws_s3.tf
+++ aws_s3.tf
@@ -1,14 +1,9 @@
 resource "aws_s3_bucket" "guardduty_s3" {
   count         = "${var.enabled ? 1 : 0}"
-  bucket        = "${var.s3_bucket_name}"
-  key           = "${var.s3_prefix}"
+  bucket        = "${var.s3_bucket_name}-${var.s3_prefix}"
   policy        = "${var.s3_bucket_policy}"
   acl           = "${var.s3_bucket_acl}"
   force_destroy = "${var.s3_bucket_force_destroy}"
-
-  lifecycle {
-    prevent_destroy = "${var.s3_prevent_destroy}"
-  }
 
   tags = "${merge(var.common_tags,
     map("Name", "${local.environment}-${var.s3_bucket_name}-S3")
