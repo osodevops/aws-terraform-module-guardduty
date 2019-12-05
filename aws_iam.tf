4002a127d30961d1dc4ed15491ea265dc9324a50
Richard Gutkowski <richard@ososolution.com>
2019-12-05 19:59:52 +0000

    updated to tf 12

--- aws_iam.tf
+++ aws_iam.tf
@@ -34,8 +34,8 @@ data "aws_iam_policy_document" "guardduty_s3" {
 
 resource "aws_iam_policy" "guardduty_s3" {
   name_prefix = "guardduty-s3-"
-  policy      = "${data.aws_iam_policy_document.guardduty_s3.json}"
-  count       = "${var.s3_enabled ? 1 : 0}"
+  policy      = data.aws_iam_policy_document.guardduty_s3.json
+  count       = var.s3_enabled ? 1 : 0
 }
 
 data "aws_iam_policy_document" "lambda_assume_role" {
@@ -51,14 +51,14 @@ data "aws_iam_policy_document" "lambda_assume_role" {
 
 resource "aws_iam_role" "guardduty_s3" {
   name               = "guardduty-s3"
-  assume_role_policy = "${data.aws_iam_policy_document.lambda_assume_role.json}"
-  count              = "${var.s3_enabled ? 1 : 0}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+  count              = var.s3_enabled ? 1 : 0
 }
 
 resource "aws_iam_role_policy_attachment" "guardduty_s3" {
-  role       = "${aws_iam_role.guardduty_s3.name}"
-  policy_arn = "${aws_iam_policy.guardduty_s3.arn}"
-  count      = "${var.s3_enabled ? 1 : 0}"
+  role       = aws_iam_role.guardduty_s3.name
+  policy_arn = aws_iam_policy.guardduty_s3.arn
+  count      = var.s3_enabled ? 1 : 0
 }
 
 data "aws_iam_policy_document" "kinesis_assume" {
@@ -69,15 +69,18 @@ data "aws_iam_policy_document" "kinesis_assume" {
 
     principals {
       type        = "Service"
-      identifiers = ["events.amazonaws.com"]
+      identifiers = [
+        "events.amazonaws.com",
+        "firehose.amazonaws.com"
+        ]
     }
   }
 }
 
 resource "aws_iam_policy" "kinesis_cwe_policy" {
   name_prefix = "guardduty-kinesis-"
-  policy = "${data.aws_iam_policy_document.kinesis_cwe_policy_doc.json}"
-  count = "${var.kinesis_enabled ? 1 : 0}"
+  policy = data.aws_iam_policy_document.kinesis_cwe_policy_doc.json
+  count = var.kinesis_enabled ? 1 : 0
 }
 
 data "aws_iam_policy_document" "kinesis_cwe_policy_doc" {
@@ -90,21 +93,48 @@ data "aws_iam_policy_document" "kinesis_cwe_policy_doc" {
     ]
 
     resources = [
-      "${var.kinesis_firehose_arn}",
+      var.kinesis_firehose_arn
     ]
   }
 }
 
+data "aws_iam_policy_document" "kinesis_delivery_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "es:DescribeElasticsearchDomain",
+      "es:DescribeElasticsearchDomains",
+      "es:DescribeElasticsearchDomainConfig",
+      "es:ESHttpPost",
+      "es:ESHttpPut"
+    ]
+
+    resource = [
+      "arn:aws:es:${var.aws_region}:${var.account_id}:domain/:${var.elasticsearchdomain}",
+      "arn:aws:es:${var.aws_region}:${var.account_id}:domain/:${var.elasticsearchdomain}/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "es:ESHttpGet"
+    ]
+      "arn:aws:es:${var.aws_region}:${var.account_id}:domain/:${var.elasticsearchdomain}",
+      "arn:aws:es:${var.aws_region}:${var.account_id}:domain/:${var.elasticsearchdomain}/*"
+  }
+}
 
 resource "aws_iam_role" "kinesis_cwe_role" {
   name = "guardduty-kinesis"
-  assume_role_policy = "${data.aws_iam_policy_document.kinesis_assume.json}"
-  count = "${var.kinesis_enabled ? 1 : 0}"
+  assume_role_policy = data.aws_iam_policy_document.kinesis_assume.json
+  count = var.kinesis_enabled ? 1 : 0
 }
 
 resource "aws_iam_role_policy_attachment" "kinesis_cwe_attachement" {
-  role = "${aws_iam_role.kinesis_cwe_role.name}"
-  policy_arn = "${aws_iam_policy.kinesis_cwe_policy.arn}"
-  count = "${var.kinesis_enabled ? 1 : 0}"
+  role = aws_iam_role.kinesis_cwe_role.name
+  policy_arn = aws_iam_policy.kinesis_cwe_policy.arn
+  count = var.kinesis_enabled ? 1 : 0
 }
-
